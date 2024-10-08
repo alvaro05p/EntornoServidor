5116f8b44b6b9eb7e49e482c13e714c9136a75af
alvaro05p <alvaro05pomg@gmail.com>
2024-10-08 08:03:03 +0200

    ejercicio7

--- PHP/UD3/ej7.php
+++ PHP/UD3/ej7.php
@@ -15,6 +15,7 @@
     if($hora < $separado[0]){
 
         $horasRestantes =  $separado[0] - $hora;
+        
 
     }else{
 
@@ -22,10 +23,30 @@
     
     }
 
-    
+    if($minuto < $separado[1]){
 
-    echo $horasRestantes . "\n";
+        $minutosRestantes = $separado[1] - $minuto;
 
+    }else{
 
+        $paraEnPunto = 60 - $minuto;
+        $minutosRestantes = $separado[1] + $paraEnPunto;
+        $horasRestantes++;
+
+    }
+
+    if($segundo < $separado[2]){
+
+        $segundosRestantes = $separado[2] - $segundo;
+
+    }else{
+
+        $paraEnPunto = 60 - $segundo;
+        $segundosRestantes = $separado[2] + $paraEnPunto;
+        $minutosRestantes++;
+
+    }
+
+    echo $horasRestantes . ":" . $minutosRestantes . ":" . $segundosRestantes . "\n";
 
 ?>
