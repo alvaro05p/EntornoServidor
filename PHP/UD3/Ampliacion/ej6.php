1e6b809b2bda269a54e29269596884ae07b7a386
alvaro05p <alvaro05pomg@gmail.com>
2024-10-13 18:58:15 +0200

    ej6 ampliación

--- PHP/UD3/Ampliacion/ej6.php
+++ PHP/UD3/Ampliacion/ej6.php
@@ -7,4 +7,32 @@
      * números en cada caso y el porcentaje de pares e impares
      */
 
+    $numeros = [];
+    $pares = 0;
+    $impares = 0;
+
+    for($i=1;$i<=8;$i++){
+        $num = readline("Numero " . $i . ": ");
+        $esPar = false;
+        if($num % 2 == 0){
+            $esPar = true;
+        }
+
+        if($esPar){
+            array_push($numeros, $num . ", par");
+            $pares++;
+        }else{
+            array_push($numeros, $num . ", impar");
+            $impares++;
+        }
+
+    }
+
+    var_dump($numeros);
+
+    $porcentajePar = ($pares * 100) / 8;
+    $porcentajeImpar = ($impares * 100) / 8;
+
+    echo "Pares: " . $pares . " ($porcentajePar%) \n";
+    echo "Impares: " . $impares . " ($porcentajeImpar%) \n"; 
 ?>
